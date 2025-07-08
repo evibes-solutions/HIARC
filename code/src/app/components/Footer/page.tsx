1600517e0dfcbf21e59b63bddbcc8f140ed1fdb4
Maheen <evibessolutions@gmail.com>
2025-07-08 21:58:57 +0500

    code

--- code/src/app/components/Footer/page.tsx
+++ code/src/app/components/Footer/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 
-export default function Home() {
+export default function Footer() {
   return (
     <div >
      
---
+++ code/src/app/components/Services/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function Services() {
+  return (
+    <div >
+     
+    </div>
+  );
+}
