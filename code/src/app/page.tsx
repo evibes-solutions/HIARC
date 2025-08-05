338a6d57ed4813c9cdfc0ca800b7098414408bef
Maheen <evibessolutions@gmail.com>
2025-08-05 15:25:19 +0500

    Update page.tsx

--- code/src/app/page.tsx
+++ code/src/app/page.tsx
@@ -2,8 +2,8 @@ import NavBar from './components/NavBar/page';
 import DotNavigation from './components/DotNavigation/page';
 import Home from './components/Home/page';
 import Services from './components/Services/page';
-//import PortfolioPage from './components/Portfolio/page';
-// import About from './components/About/page';
+import PortfolioPage from './components/Portfolio/page';
+import About from './components/About/page';
 import Contact from './components/Contact/page';
 import Footer from './components/Footer/page';
 
@@ -19,12 +19,12 @@ export default function HomePage() {
       <section id="services">
         <Services />
       </section>
-      {/* <section id="portfolio">
+      <section id="portfolio">
         <PortfolioPage />
-      </section> */}
-      {/* <section id="about">
+      </section>
+      <section id="about">
         <About />
-      </section> */}
+      </section>
       <section id="contact">
         <Contact />
       </section>
